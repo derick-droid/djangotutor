8ef04a82f4104ca724e811728acd817567dae0fd
derick-droid <derrickokinda9@gmail.com>
2022-02-19 13:39:35 +0300

    design models tutorials

--- django-tutorials/hello/models.py
+++ django-tutorials/hello/models.py
@@ -1,3 +1,23 @@
+from operator import mod
+from unicodedata import name
 from django.db import models
 
-# Create your models here.
+class Dreamreal(models.Model):
+    website = models.CharField(max_length=50)
+    mails = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    phonenumber = models.IntegerField
+    
+    
+    class meta:
+        db_table = "dreamreal"
+        
+        
+class Online(models.Model):
+    domain = models.CharField(max_length=30)
+    
+    class meta:
+        dt_table = "online"
+        
+        
+    
--- django-tutorials/hello/views.py
+++ django-tutorials/hello/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from hello.models import Dreamreal
+
 # http response
 def welcome(request):
     text = "<h1> welcome to my app </h1>"
@@ -13,3 +14,35 @@ def home(request):
 
 def name(request, name):
     return HttpResponse(f"hello,{name.capitalize()}")
+
+# Manipulating Data (CRUD)
+def crudops(request):
+    #  creating entry
+    dreamreal = Dreamreal(website = "www.polo.com",  mail = "sorex@polo.com",ame = "sorex",phonenumber = "002376970")
+    dreamreal.save()
+    
+    # read all entries
+    objects  = Dreamreal.objets.all()
+    res = "printing all Dreamreal entries in DB: <br> "
+    for elt in objects:
+        res += elt.name + "<br>"
+        
+    #  read a specific entry
+    
+    sorex = Dreamreal.objects.get(name = "sorex")
+    res += "printing one entry <br> "
+    res += sorex.name
+    #  delete an entry
+    res+= "<br> Deleting an entry <br>"
+    sorex.delete
+    
+    # update
+    dreamreal = Dreamreal(website = "www.polo.com", mail = "sorex@polo.com", 
+      name = "sorex", phonenumber = "002376970")
+    
+    dreamreal.save()
+    res+= "updating entry <br> "
+    dreamreal = Dreamreal.objects.get(name = "sorex")
+    dreamreal = "thierry"
+    dreamreal.save()
+    return HttpResponse(res)
