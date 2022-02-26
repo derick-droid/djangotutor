78305ce5d722ff1e42cd815d2f4e50c0080f0802
derick-droid <derrickokinda9@gmail.com>
2022-02-26 14:41:14 +0300

    page redirection in django

--- django-tutorials/morning/urls.py
+++ django-tutorials/morning/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path("days", views.displayfor, name="days"),
-    path("good", views.good, name="good"),
-    path("greet", views.greet, name="greet"),
-    path("date", views.date, name="date"),
-    path("redirect", views.redirect, name = "redirect"),
-    path("<str:articleId>/", views.viewArticle, name = "viewarticle"),
-   ]
+    path("days/", views.displayfor, name="days"),
+    path("good/", views.good, name="good"),
+    path("greet/", views.greet, name="greet"),
+    path("date/", views.date, name="date"),
+    path("redirect/", views.redirect, name = "redirect"),
+    path("django/", views.django, name="django"),
+]
--- django-tutorials/morning/views.py
+++ django-tutorials/morning/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 import datetime
 
@@ -13,20 +13,14 @@ def greet(request):
 def date(request):
     today = datetime.datetime.now().date()
     return render(request, 'morning/date.html', {'today':today})
+
 def displayfor(request):
     daysweek = ["monday", "tuesday", "wednesday", "thursday", "friday","saturday", "sunday"]
     return render(request, 'morning/for.html', {'daysweek': daysweek})
-#  page redirect
 
-def redirect(request):
+#  page redirect
+def django(request):
     today = datetime.datetime.now().date()
     daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    return render(request, "morning/index.html", {"today" : today, "days_of_week" : daysOfWeek})
-
-
-def viewArticle(request, articleId):
-   """ A view that display an article based on his ID"""
-   text = "Displaying article Number : %s" %articleId
-   return HttpResponse(text)
-
+    return redirect("https://www.djangoproject.com")
 
